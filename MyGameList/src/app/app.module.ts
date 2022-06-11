8ab47f0b39b39933c0fe0009d4ab5f949c6b663b
BhrunoB8 <bhruno.roan@yahoo.com>
2022-06-10 21:11:32 -0300

    Adição de rotas

--- MyGameList/src/app/app.module.ts
+++ MyGameList/src/app/app.module.ts
@@ -13,6 +13,7 @@ import {MatCardModule} from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import {MatButtonModule} from '@angular/material/button';
     AppComponent,
     FooterComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
