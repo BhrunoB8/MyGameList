88c7ad833830d4698815878636d4d0f89f42c334
gustavo-diogo <gustavost1y@gmail.com>
2022-08-08 12:25:38 -0300

    feat: service api route, now there's one for the entire project

---
+++ src/services/api/api.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor() { }
+
+  getRoute() {
+    return 'http://192.168.1.118:3030'
+  }
+
+
+}
