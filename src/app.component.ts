521b1ed469dcd283727c08b3bc9bd221b31c9e70
gustavo-diogo <gustavost1y@gmail.com>
2022-08-08 12:26:10 -0300

    feat: on opening the app, you request your profile informations

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,6 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Component } from '@angular/core';
 import { Chart } from 'chart.js';
+import { CookieService } from 'ngx-cookie-service';
+import { ApiService } from './services/api/api.service';
+import { ProfileInfoService } from './services/profile/profile-info.service';
 
 
 
@@ -10,6 +14,19 @@ import { Chart } from 'chart.js';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent {
   title = 'MyGameList';
+
+  constructor(private http: HttpClient, private api: ApiService, private profileInfo: ProfileInfoService, private cookie: CookieService) { }
+
+  ngOnInit(): void {
+    this.http.get<any>(`${this.api.getRoute()}/user`,{ headers: { Authorization: 'Bearer ' + this.cookie.get('token') }}).subscribe(resultado => {
+
+      this.profileInfo.setFavoriteUserGames(resultado.games)
+      this.profileInfo.setUserInfo(resultado.user)
+      console.log(this.profileInfo.getFavoriteUserGames())
+
+    }, erro => { })
+  }
+
 }
