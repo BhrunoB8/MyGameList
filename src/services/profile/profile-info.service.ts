f68dd2a07d8cb17564ec11964e6405937df52bca
gustavo-diogo <gustavost1y@gmail.com>
2022-08-08 12:25:19 -0300

    feat: service for profile, now can be accessed from anywhere

---
+++ src/services/profile/profile-info.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileInfoService {
+
+  constructor() { }
+   userInfo: any = ''
+   favoriteUserGames: any[] = []
+
+
+  setUserInfo(info: any) {
+    this.userInfo = info
+  }
+
+  getUserInfo() {
+    return this.userInfo;
+  }
+
+  setFavoriteUserGames(info: any) {
+    this.favoriteUserGames = info
+  }
+
+  getFavoriteUserGames() {
+    return this.favoriteUserGames;
+  }
+
+
+
+}
